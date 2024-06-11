40ba7c9a87839525f4fe21fb03cd5dae9a6f54b2
teixeira26 <tu-nuevo-email@example.com>
2024-06-11 16:56:58 -0300

    add responsive and change of grafics

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,2 +1,2 @@
-export const BACKEND_URL = 'http://localhost:3003'
-//  export const BACKEND_URL = 'https://backendsistemas.byderm.com.ar'
+// export const BACKEND_URL = 'http://localhost:3003'
+ export const BACKEND_URL = 'https://backendsistemas.byderm.com.ar'
